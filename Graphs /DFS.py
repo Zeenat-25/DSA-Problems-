44d339c9665df35e896931cf63fa0c4d9f3485f1
Zeenat Ansari <libraskingdom@gmail.com>
2026-07-11 20:01:21 +0530

    Create DFS.py

---
+++ Graphs /DFS.py
@@ -0,0 +1,56 @@
+ graph = {
+     'Oradea' : ['Zerind','Sibiu',],
+     'Arad' : ['Sibiu' , 'Timisoara' , 'Zerind'],
+     'Zerind' : ['Arad' , 'Oradea'],
+     'Timisoara' : ['Lugoj' , 'Arad'],
+     'Lugoj' : ['Mechadia','Timisoara'],
+     'Mechadia' : ['Lugoj','Drobeta'],
+     'Drobeta' : ['Mechadia' , 'Craiova'],
+     'Craiova' : ['Drobeta' , 'Rimnicu Vilcea', 'Pitesti'],
+     'Pitesti' : ['Craiova' , 'Rimnicu Vilcea' , 'Bucharest'],
+     'Rimnicu Vilcea' : ['Pitesti' , 'Craiova' , 'Sibiu'],
+     'Sibiu' : ['Arad' , 'Oradea' , 'Rimnicu Vilcea' , 'Fagaras'],
+     'Fagaras' : ['Sibiu' , 'Bucharest'],
+     'Bucharest' : ['Fagaras' , 'Pitesti' , 'Giurgiu' , 'Urziceni'],
+     'Giurgiu' : ['Bucharest'],
+     'Urziceni' : ['Bucharest' , 'Hirsova' , 'Vaslui'],
+     'Hirsova' : ['Urziceni' , 'Eforie'],
+     'Eforie' : ['Hirsova'],
+     'Vaslui' : ['Urziceni' , 'Iasi'],
+     'Iasi' : ['Vaslui' , 'Neamt'],
+     'Neamt' : ['Iasi']
+
+
+ }
+
+ def dfs(graph , start , goal) :
+  stack = [[start]]
+  visited = []
+  while stack :
+    path = stack.pop()
+    node = path[-1]
+
+    if node == goal :
+      return path
+
+    if node not in visited :
+      visited.append(node)
+
+
+      for neighbour in reversed(graph[node]) :
+        new_path = list(path)
+        new_path.append(neighbour)
+        stack.append(new_path)
+
+  return None
+
+
+start = 'Oradea'
+goal = 'Hirsova'
+
+result = dfs(graph , start , goal)
+
+if result :
+  print("Path Found : "," -> ", " -> ".join(result))
+else :
+  print("Path Not Found")
